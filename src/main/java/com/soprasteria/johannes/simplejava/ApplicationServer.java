a7e64b9809adaf99a2d399acfd1a360a8c51b5bd
Johannes Brodwall <johannes@brodwall.com>
2023-07-31 01:49:55 +0200

    ApplicationServer and ApplicationServerTest (starts, but returns 404)

--- src/main/java/com/soprasteria/johannes/simplejava/ApplicationServer.java
+++ src/main/java/com/soprasteria/johannes/simplejava/ApplicationServer.java
@@ -1,22 +1,26 @@
 package com.soprasteria.johannes.simplejava;
 
+import org.eclipse.jetty.server.Server;
+
 import java.net.URI;
 
 public class ApplicationServer {
 
+    private final Server server;
+
     public ApplicationServer(int port) {
-
+        server = new Server(port);
     }
 
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-    }
-
-    public void start() {
-
+    public void start() throws Exception {
+        server.start();
     }
 
     public URI getURI() {
-        return null;
+        return server.getURI();
+    }
+
+    public static void main(String[] args) throws Exception {
+        new ApplicationServer(8080).start();
     }
 }
--- src/test/java/com/soprasteria/johannes/simplejava/ApplicationServerTest.java
+++ src/test/java/com/soprasteria/johannes/simplejava/ApplicationServerTest.java
@@ -2,7 +2,6 @@ package com.soprasteria.johannes.simplejava;
 
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -12,7 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class ApplicationServerTest {
 
     @Test
-    void shouldStartOnSpecifiedPort() throws IOException, InterruptedException {
+    void shouldStartOnSpecifiedPort() throws Exception {
         var server = new ApplicationServer(0);
         server.start();
 
