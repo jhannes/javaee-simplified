02e5462a3328ec50828df213226e280a280fe45e
Johannes Brodwall <johannes@brodwall.com>
2023-07-31 02:10:11 +0200

    Logging

--- src/main/java/com/soprasteria/johannes/simplejava/ApplicationServer.java
+++ src/main/java/com/soprasteria/johannes/simplejava/ApplicationServer.java
@@ -1,12 +1,14 @@
 package com.soprasteria.johannes.simplejava;
 
 import com.soprasteria.johannes.simplejava.server.ContentServlet;
+import lombok.extern.slf4j.Slf4j;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
 import java.net.URI;
 
+@Slf4j
 public class ApplicationServer {
 
     private final Server server;
@@ -29,6 +31,6 @@ public class ApplicationServer {
     public static void main(String[] args) throws Exception {
         var server = new ApplicationServer(8080);
         server.start();
-        System.out.println("Started on " + server.getURI());
+        log.info("Started on {}", server.getURI());
     }
 }
