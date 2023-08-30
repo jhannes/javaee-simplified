168b815ef687fb9d887acdf2eedff791281c2613
Johannes Brodwall <johannes@brodwall.com>
2023-08-30 22:16:45 +0200

    initial frontend application styling

--- src/main/frontend/index.tsx
+++ src/main/frontend/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import * as ReactDOM from "react-dom/client";
 import { TodoApplication } from "./components/todoApplication/todoApplication";
 
-const root = ReactDOM.createRoot(document.getElementById("root")!);
+import "./application.css";
 
+const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(<TodoApplication />);
