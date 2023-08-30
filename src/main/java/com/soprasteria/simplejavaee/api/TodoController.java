3d557a406ae192c8c566546e07a236a8df069a10
Johannes Brodwall <johannes@brodwall.com>
2023-08-30 22:52:38 +0200

    backend implementation of createTodo (without database)

--- src/main/java/com/soprasteria/simplejavaee/api/TodoController.java
+++ src/main/java/com/soprasteria/simplejavaee/api/TodoController.java
@@ -3,19 +3,28 @@ package com.soprasteria.simplejavaee.api;
 import com.soprasteria.generated.simplejavaee.model.SampleModelData;
 import com.soprasteria.generated.simplejavaee.model.TodoDto;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Path("/todos")
 public class TodoController {
 
-    private final SampleModelData sampleModelData = new SampleModelData(-1);
+    private static final SampleModelData sampleModelData = new SampleModelData(-1);
+    private static final List<TodoDto> todoDtos = new ArrayList<>(sampleModelData.sampleListOfTodoDto());
 
     @GET
     public List<TodoDto> listTodos() throws InterruptedException {
         Thread.sleep(500);
-        return sampleModelData.sampleListOfTodoDto();
+        return todoDtos;
+    }
+
+    @POST
+    public void createTodo(TodoDto todo) throws InterruptedException {
+        Thread.sleep(100);
+        todoDtos.add(todo);
     }
 
 }
