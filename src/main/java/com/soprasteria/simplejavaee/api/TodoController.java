607622b6796df779ce7bdb30a01403385393e49f
Johannes Brodwall <johannes@brodwall.com>
2023-08-31 01:16:03 +0200

    Clean up todo controller code

--- src/main/java/com/soprasteria/simplejavaee/api/TodoController.java
+++ src/main/java/com/soprasteria/simplejavaee/api/TodoController.java
@@ -1,6 +1,5 @@
 package com.soprasteria.simplejavaee.api;
 
-import com.soprasteria.generated.simplejavaee.model.SampleModelData;
 import com.soprasteria.generated.simplejavaee.model.TodoDto;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
@@ -8,7 +7,6 @@ import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import org.fluentjdbc.DbContext;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Path("/todos")
@@ -16,23 +14,18 @@ public class TodoController {
 
     private final TodoDao todoDao;
 
-    private static final SampleModelData sampleModelData = new SampleModelData(-1);
-    private static final List<TodoDto> todoDtos = new ArrayList<>(sampleModelData.sampleListOfTodoDto());
-
     @Inject
     public TodoController(DbContext dbContext) {
         todoDao = new TodoDao(dbContext);
     }
 
     @GET
-    public List<TodoDto> listTodos() throws InterruptedException {
+    public List<TodoDto> listTodos() {
         return todoDao.list();
     }
 
     @POST
-    public void createTodo(TodoDto todo) throws InterruptedException {
-        Thread.sleep(100);
-        todoDtos.add(todo);
+    public void createTodo(TodoDto todo) {
         todoDao.save(todo);
     }
 
