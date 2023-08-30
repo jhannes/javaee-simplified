2cc6f3cad2b04e1aded1437c12b297a97be2e33a
Johannes Brodwall <johannes@brodwall.com>
2023-08-30 22:50:22 +0200

    frontend implementation of createTodo

--- src/main/frontend/components/todoApplication/newTodoForm.tsx
+++ src/main/frontend/components/todoApplication/newTodoForm.tsx
@@ -1,9 +1,46 @@
-import React from "react";
+import React, { FormEvent, useState } from "react";
+import {
+  servers,
+  TodoDto,
+} from "../../../../../target/generated-sources/openapi-typescript";
+import { v4 as uuidv4 } from "uuid";
+import { useNavigate } from "react-router-dom";
 
 export function NewTodoForm() {
+  const [todoDto, setTodoDto] = useState<TodoDto>({
+    title: "",
+    state: "WAITING",
+    id: uuidv4(),
+  });
+  const navigate = useNavigate();
+  async function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    await servers.current.todosApi.createTodo({
+      todoDto,
+    });
+    navigate("/todos");
+  }
   return (
-    <>
+    <form onSubmit={handleSubmit}>
       <h2>Add new task</h2>
-    </>
+
+      <div>
+        <label>
+          Title:
+          <br />
+          <input
+            autoFocus={true}
+            type="text"
+            value={todoDto.title}
+            onChange={(e) =>
+              setTodoDto((old) => ({ ...old, title: e.target.value }))
+            }
+          />
+        </label>
+      </div>
+      <div>
+        <button>Submit</button>
+      </div>
+    </form>
   );
 }
