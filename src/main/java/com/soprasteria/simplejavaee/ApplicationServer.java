74bad493d757f18465e3f41070634722100ee9c6
Johannes Brodwall <johannes@brodwall.com>
2023-09-02 11:44:17 +0200

    Adding a simple servlet

--- src/main/java/com/soprasteria/simplejavaee/ApplicationServer.java
+++ src/main/java/com/soprasteria/simplejavaee/ApplicationServer.java
@@ -1,12 +1,19 @@
 package com.soprasteria.simplejavaee;
 
+import com.soprasteria.simplejavaee.api.MyLittleServlet;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 
 public class ApplicationServer {
     private final Server server;
 
     public ApplicationServer(int port) {
         this.server = new Server(port);
+
+        var context = new ServletContextHandler();
+        context.addServlet(new ServletHolder(new MyLittleServlet()), "/api/*");
+        server.setHandler(context);
     }
 
     public static void main(String[] args) throws Exception {
---
+++ src/main/java/com/soprasteria/simplejavaee/api/MyLittleServlet.java
@@ -0,0 +1,15 @@
+package com.soprasteria.simplejavaee.api;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class MyLittleServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("Hello JavaZone");
+    }
+}
