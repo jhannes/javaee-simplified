1a21a780ff49fc88b11ea9697201774da22b249b
Johannes Brodwall <johannes@brodwall.com>
2023-09-02 11:46:53 +0200

    Adding support for static content

--- src/main/java/com/soprasteria/simplejavaee/ApplicationServer.java
+++ src/main/java/com/soprasteria/simplejavaee/ApplicationServer.java
@@ -2,8 +2,10 @@ package com.soprasteria.simplejavaee;
 
 import com.soprasteria.simplejavaee.api.MyLittleServlet;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.util.resource.Resource;
 
 public class ApplicationServer {
     private final Server server;
@@ -12,7 +14,9 @@ public class ApplicationServer {
         this.server = new Server(port);
 
         var context = new ServletContextHandler();
+        context.setBaseResource(Resource.newClassPathResource("/webapp"));
         context.addServlet(new ServletHolder(new MyLittleServlet()), "/api/*");
+        context.addServlet(new ServletHolder(new DefaultServlet()), "/*");
         server.setHandler(context);
     }
 
