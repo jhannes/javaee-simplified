acde52f54fa888f1944806339e8ce599a5a5abc0
Johannes Brodwall <johannes@brodwall.com>
2023-09-17 12:32:46 +0200

    Start Jetty Server in ApplicationServer

--- src/main/java/com/soprasteria/javaeesimplified/ApplicationServer.java
+++ src/main/java/com/soprasteria/javaeesimplified/ApplicationServer.java
@@ -1,7 +1,19 @@
 package com.soprasteria.javaeesimplified;
 
+import org.eclipse.jetty.server.Server;
+
 public class ApplicationServer {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    private final Server server;
+
+    public ApplicationServer(int port) {
+        this.server = new Server(port);
+    }
+
+    public static void main(String[] args) throws Exception {
+        new ApplicationServer(8080).start();
+    }
+
+    private void start() throws Exception {
+        server.start();
     }
 }
