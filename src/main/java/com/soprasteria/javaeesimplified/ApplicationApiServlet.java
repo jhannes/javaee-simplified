8bcb112abc6c39b9bd95bbce361ac24b6829390f
Johannes Brodwall <johannes@brodwall.com>
2023-09-17 15:31:12 +0200

    Initialize tasks in ApplicationApiServlet constructor

--- src/main/java/com/soprasteria/javaeesimplified/ApplicationApiServlet.java
+++ src/main/java/com/soprasteria/javaeesimplified/ApplicationApiServlet.java
@@ -1,25 +1,36 @@
 package com.soprasteria.javaeesimplified;
 
 import com.soprasteria.generated.javaeesimplified.model.SampleModelData;
+import com.soprasteria.generated.javaeesimplified.model.TodoDto;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.jsonbuddy.pojo.JsonGenerator;
 
 import java.io.IOException;
+import java.util.Map;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 class ApplicationApiServlet extends HttpServlet {
 
     private final JsonGenerator jsonb = new JsonGenerator();
+    private final Map<UUID, TodoDto> tasks;
+
+    public ApplicationApiServlet() {
+        var sampleData = new SampleModelData(2);
+        tasks = sampleData.sampleList(sampleData::sampleTodoDto, 5, 20)
+                .stream().collect(Collectors.toMap(TodoDto::getId, todo -> todo));
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        var sampleData = new SampleModelData(2);
         if (req.getPathInfo().equals("/tasks")) {
-            var response = sampleData.sampleListOfTodoDto();
+            var response = tasks.values();
             resp.setContentType("application/json");
             jsonb.generateNode(response).toJson(resp.getWriter());
         } else if (req.getPathInfo().equals("/login")) {
+            var sampleData = new SampleModelData(2);
             var response = sampleData.sampleUserinfoDto()
                     .displayName(sampleData.randomPersonName());
             resp.setContentType("application/json");
