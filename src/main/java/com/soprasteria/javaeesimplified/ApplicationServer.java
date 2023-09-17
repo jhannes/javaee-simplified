5131e44932bcb8177fcfe8d5a74fb152993e9620
Johannes Brodwall <johannes@brodwall.com>
2023-09-17 12:34:35 +0200

    Add hello-world servlet to context

--- src/main/java/com/soprasteria/javaeesimplified/ApplicationServer.java
+++ src/main/java/com/soprasteria/javaeesimplified/ApplicationServer.java
@@ -1,12 +1,28 @@
 package com.soprasteria.javaeesimplified;
 
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+import java.io.IOException;
 
 public class ApplicationServer {
     private final Server server;
 
     public ApplicationServer(int port) {
-        this.server = new Server(port);
+        var context = new ServletContextHandler();
+        context.addServlet(new ServletHolder(new HttpServlet() {
+            @Override
+            protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+                resp.getWriter().write("Hello World");
+            }
+        }), "/api/*");
+
+        server = new Server(port);
+        server.setHandler(context);
     }
 
     public static void main(String[] args) throws Exception {
