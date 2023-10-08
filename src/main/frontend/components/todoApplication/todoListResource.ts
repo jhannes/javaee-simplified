3799b602ec91221cd045393cb360814ef82335b2
Johannes Brodwall <johannes@brodwall.com>
2023-10-08 23:09:58 +0200

    `npm run prettier`

--- src/main/frontend/components/todoApplication/todoListResource.ts
+++ src/main/frontend/components/todoApplication/todoListResource.ts
@@ -1,4 +1,7 @@
 import { servers } from "api";
 import { getAsyncResource } from "@mittwald/react-use-promise";
 
-export const todoListResource = getAsyncResource(() => servers.current.todosApi.listTodos(), []);
+export const todoListResource = getAsyncResource(
+  () => servers.current.todosApi.listTodos(),
+  [],
+);
