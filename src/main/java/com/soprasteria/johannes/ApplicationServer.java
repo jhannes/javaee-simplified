7ff8879765f719a72ff4070a27151aa964cdcc80
Johannes Brodwall <johannes@brodwall.com>
2023-10-13 10:30:13 +0200

    End of talk

--- src/main/java/com/soprasteria/johannes/ApplicationServer.java
+++ src/main/java/com/soprasteria/johannes/ApplicationServer.java
@@ -1,2 +1,36 @@
-package com.soprasteria.johannes;public class ApplicationServer {
+package com.soprasteria.johannes;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.DefaultServlet;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.util.resource.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ApplicationServer {
+
+    private final Server server;
+
+    private static final Logger log = LoggerFactory.getLogger(ApplicationServer.class);
+
+    public ApplicationServer(int port) {
+        server = new Server(port);
+
+        var handler = new ServletContextHandler();
+        handler.setBaseResource(Resource.newClassPathResource("/webapp"));
+        handler.addServlet(new ServletHolder(new MyLittleServlet()), "/api/*");
+        handler.addServlet(new ServletHolder(new DefaultServlet()), "/*");
+        server.setHandler(handler);
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        new ApplicationServer(11080).start();
+    }
+
+    private void start() throws Exception {
+        server.start();
+        log.info("server started at http://localhost:{}", server.getURI().getPort());
+    }
 }
--- src/main/java/com/soprasteria/johannes/MyLittleServlet.java
+++ src/main/java/com/soprasteria/johannes/MyLittleServlet.java
@@ -1,2 +1,24 @@
-package com.soprasteria.johannes;public class MyLittleServlet {
+package com.soprasteria.johannes;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class MyLittleServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        var pathInfo = req.getPathInfo();
+        var author = req.getParameter("author");
+        resp.setContentType("text/plain");
+
+        if ("/books".equals(pathInfo)) {
+            resp.getWriter().write("Books by " + author);
+        } else {
+            resp.getWriter().write("<h1>Hello World</h1>");
+        }
+    }
 }
